1436918f646e67c120ce31ef98fb4e793a298152
Alex Nascimento <alexdavi150@gmail.com>
2024-12-17 19:42:05 -0300

    style: use a separate folder for testing

--- task1/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class URLTests(TestCase):
-
-  def test_homepage_exists(self):
-    response = self.client.get('/')
-    self.assertEqual(response.status_code, 200)
-  
