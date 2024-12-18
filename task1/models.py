8091e1a56b60b5da3b169aab1a85b9b5f14febe0
Alex Nascimento <alexdavi150@gmail.com>
2024-12-18 08:17:36 -0300

    refact: add slug field to post model

--- task1/models.py
+++ task1/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
 
   title = models.CharField(max_length=255)
   content = models.TextField()
+  slug = models.SlugField(max_length=255)
 
   likes = models.ManyToManyField(
     User,
@@ -12,6 +14,9 @@ class Post(models.Model):
     default = None,
     blank = True
   )
+
+  def get_absolute_url(self):
+    return reverse('task1:post_single', args=[self.slug])
 
   def __str__(self):
     return self.title
