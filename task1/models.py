ceb2ddd4851c2f3510bf000ac052215e283bda53
Alex Nascimento <alexdavi150@gmail.com>
2024-12-18 07:25:31 -0300

    feat: add post model

--- task1/models.py
+++ task1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+
+  title = models.CharField(max_length=255)
+  content = models.TextField()
+
+  likes = models.ManyToManyField(
+    User,
+    related_name = 'likes',
+    default = None,
+    blank = True
+  )
+
+  def __str__(self):
+    return self.title
