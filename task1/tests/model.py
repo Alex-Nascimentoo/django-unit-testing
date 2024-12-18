38a8caf2373f2456b0feed004c1ba8154a998726
Alex Nascimento <alexdavi150@gmail.com>
2024-12-18 08:08:03 -0300

    refact: use setUpTestData function instead of setUp

--- task1/tests/model.py
+++ task1/tests/model.py
@@ -4,23 +4,24 @@ from django.contrib.auth.models import User
 
 class TestTask1Models(TestCase):
 
-  def setUp(self):
-    self.user = User.objects.create_user(
+  @classmethod
+  def setUpTestData(cls):
+    user = User.objects.create_user(
       username = 'testuser',
       password = 'pass',
     )
 
-    self.user2 = User.objects.create_user(
+    user2 = User.objects.create_user(
       username = 'testuser2',
       password = 'pass2',
     )
 
-    self.post = Post.objects.create(
+    cls.post = Post.objects.create(
       title = 'Test Post',
       content = 'Test Content',
     )
 
-    self.post.likes.set([self.user.pk, self.user2.pk])
+    cls.post.likes.set([user.pk, user2.pk])
 
 
   def test_post_str(self):
